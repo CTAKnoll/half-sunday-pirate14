b1083c1491a43aa5a2d813216159a0b62f74ddc1
CTAKnoll <ctk140030@utdallas.edu>
2024-01-23 23:54:20 -0800

    Added a cool font; resolution buttons now work

---
+++ Assets/ResolutionButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Services;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ResolutionButton : UIInteractable
+{
+    public int2 Resolution;
+    private UIDriver UiDriver;
+
+    void Start()
+    {
+        ServiceLocator.TryGetService(out UiDriver);
+        int maxSupportedWidth = Screen.resolutions.Max(res => res.width);
+        int maxSupportedHeight = Screen.resolutions.Max(res => res.height);
+        
+        // if the resolution would be breaking, don't show it
+        if (maxSupportedWidth < Resolution.x || maxSupportedHeight < Resolution.y)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        UiDriver.RegisterForTap(this, () => Screen.SetResolution(Resolution.x, Resolution.y, false));
+    }
+    
+    
+}
