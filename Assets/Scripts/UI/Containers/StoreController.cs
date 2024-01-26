7ae1df3d82eae041c44001fa8dd1d6fb7c03207f
CTAKnoll <ctk140030@utdallas.edu>
2024-01-26 14:19:35 -0800

    Fix for pollenated tulips and slowed store refresh slightly

--- Assets/Scripts/UI/Containers/StoreController.cs
+++ Assets/Scripts/UI/Containers/StoreController.cs
@@ -35,8 +35,8 @@ namespace UI.Containers
         public StoreController(StoreView view, StoreModel model) : base(view, model)
         {
             Server = new Store(View.SlotControllers);
-            Timeline.AddRecurring(this, RefreshStore, TimeSpan.FromDays(15));
-            Timeline.AddRecurring(this, ChangeMerchant, TimeSpan.FromDays(60));
+            Timeline.AddRecurring(this, RefreshStore, TimeSpan.FromDays(40));
+            Timeline.AddRecurring(this, ChangeMerchant, TimeSpan.FromDays(90));
         }
 
         public void InsertMerchantImages(Dictionary<MerchantOrigin, Sprite> sprites)
